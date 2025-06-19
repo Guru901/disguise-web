e7f3cc97ca0e353c9b6c7bc0ac537b5294f62016
Guru901 <avinashkaur842@gmail.com>
2025-06-19 20:37:49 +0530

    Enhance post upload functionality by integrating UploadButton and UploadDropzone components; update image upload handling with toast notifications. Adjust layout in PostUploadForm for improved user experience and increase title character limit in uploadPostSchema from 20 to 30. Modify Post component styles for better responsiveness.

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -4,9 +4,9 @@ import { Card } from "@/components/ui/card";
 
 export default function Post() {
   return (
-    <Card className="mx-auto mt-2 w-[calc(100%-1rem)] gap-4 px-4">
+    <Card className="mx-auto mt-2 flex min-h-screen w-[calc(100%-1rem)] flex-col gap-4 p-0">
       <Navbar />
-      <div className="mx-auto max-w-2xl space-y-6 py-12">
+      <div className="m-auto max-w-2xl space-y-6 py-12">
         <div className="space-y-2 text-center">
           <h1 className="text-xl font-bold">Create a New Post</h1>
           <p className="text-muted-foreground text-md">
