0da22b825b50341546f77365d6b4de2251f19d3f
Guru901 <avinashkaur842@gmail.com>
2025-06-20 23:14:43 +0530

    Add commentAddSchema for comment validation and refactor postRouter to utilize data access layer. Streamlined API methods for fetching posts and comments, enhancing code maintainability and readability.

---
+++ src/dal/post.ts
@@ -0,0 +1,291 @@
+import type { TCommentAddSchema, TUploadPostSchema } from "@/lib/schemas";
+import { db } from "@/server/db";
+import { commentSchema, postSchema, userSchema } from "@/server/db/schema";
+import { ConsoleLogWriter, desc, eq, sql } from "drizzle-orm";
+
+export async function getFeed() {
+  try {
+    const results = await db
+      .select()
+      .from(postSchema)
+      .where(eq(postSchema.isPublic, true))
+      .leftJoin(userSchema, eq(userSchema.id, postSchema.createdBy))
+      .orderBy(desc(postSchema.createdAt));
+
+    return results.map((row) => ({
+      ...row.posts,
+      createdBy: row.users,
+    }));
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+export async function getLoggedInUserPost(userId: string) {
+  try {
+    const results = await db
+      .select()
+      .from(postSchema)
+      .where(eq(postSchema.createdBy, userId))
+      .orderBy(desc(postSchema.createdAt));
+
+    return results;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+export async function getPostById(postId: string) {
+  const results = await db
+    .select()
+    .from(postSchema)
+    .where(eq(postSchema.id, postId));
+
+  const post = results[0];
+  if (!post) return null;
+
+  const userResults = await db
+    .select()
+    .from(userSchema)
+    .where(eq(userSchema.id, post.createdBy!));
+
+  const user = userResults[0] ?? null;
+
+  return {
+    ...post,
+    createdBy: user,
+  };
+}
+
+export async function createPost(input: TUploadPostSchema) {
+  const post = await db
+    .insert(postSchema)
+    .values({
+      title: input.title,
+      content: input.content ?? null,
+      image: input.image ?? null,
+      isPublic: input.isPublic,
+      topic: input.topic ?? "",
+      createdBy: input.author,
+      likes: [],
+      disLikes: [],
+    })
+    .returning({ id: postSchema.id });
+
+  await db
+    .update(userSchema)
+    .set({
+      posts: sql`array_append(${userSchema.posts}, ${post[0]!.id})`,
+    })
+    .where(eq(userSchema.id, input.author));
+
+  if (Array.isArray(post) && post.length > 0 && post[0]?.id) {
+    return {
+      success: true,
+      message: "Post created successfully",
+      postId: post[0].id,
+    };
+  } else {
+    return {
+      success: false,
+      message: "Failed to create post",
+    };
+  }
+}
+
+export async function likePost(userId: string, postId: string) {
+  try {
+    await db
+      .update(postSchema)
+      .set({
+        likes: sql`array_append(${postSchema.likes}, ${userId})`,
+      })
+      .where(eq(postSchema.id, postId));
+
+    return {
+      success: true,
+      message: "Post Liked successfully",
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      success: false,
+      message: "Failed to like post",
+    };
+  }
+}
+
+export async function unlikePost(userId: string, postId: string) {
+  try {
+    await db
+      .update(postSchema)
+      .set({
+        likes: sql`array_remove(${postSchema.likes}, ${userId})`,
+      })
+      .where(eq(postSchema.id, postId));
+
+    return {
+      success: true,
+      message: "Post Unliked successfully",
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      success: false,
+      message: "Failed to unlike post",
+    };
+  }
+}
+
+export async function likeAndUndislikePost(userId: string, postId: string) {
+  try {
+    await db
+      .update(postSchema)
+      .set({
+        likes: sql`array_append(${postSchema.likes}, ${userId})`,
+        disLikes: sql`array_remove(${postSchema.disLikes}, ${userId})`,
+      })
+      .where(eq(postSchema.id, postId));
+
+    return {
+      success: true,
+      message: "Post liked and undisliked successfully",
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      success: false,
+      message: "Failed to like and undislike post",
+    };
+  }
+}
+
+export async function dislikePost(userId: string, postId: string) {
+  try {
+    await db
+      .update(postSchema)
+      .set({
+        disLikes: sql`array_append(${postSchema.disLikes}, ${userId})`,
+      })
+      .where(eq(postSchema.id, postId));
+
+    return {
+      success: true,
+      message: "Post Disliked successfully",
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      success: false,
+      message: "Failed to dislike post",
+    };
+  }
+}
+
+export async function undislikePost(userId: string, postId: string) {
+  try {
+    await db
+      .update(postSchema)
+      .set({
+        disLikes: sql`array_remove(${postSchema.disLikes}, ${userId})`,
+      })
+      .where(eq(postSchema.id, postId));
+
+    return {
+      success: true,
+      message: "Post Undisliked successfully",
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      success: false,
+      message: "Failed to undislike post",
+    };
+  }
+}
+
+export async function dislikeAndUnlikePost(userId: string, postId: string) {
+  try {
+    await db
+      .update(postSchema)
+      .set({
+        disLikes: sql`array_append(${postSchema.disLikes}, ${userId})`,
+        likes: sql`array_remove(${postSchema.likes}, ${userId})`,
+      })
+      .where(eq(postSchema.id, postId));
+
+    return {
+      success: true,
+      message: "Post disliked and unliked successfully",
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      success: false,
+      message: "Failed to dislike and unlike post",
+    };
+  }
+}
+
+export async function getCommentsByPostId(postId: string) {
+  try {
+    const results = await db
+      .select()
+      .from(commentSchema)
+      .where(eq(commentSchema.post, postId))
+      .orderBy(desc(commentSchema.createdAt))
+      .leftJoin(userSchema, eq(userSchema.id, commentSchema.author));
+
+    return results;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+export async function addComment(input: TCommentAddSchema, userId: string) {
+  let comment;
+
+  if (input.isAReply) {
+    comment = await db
+      .insert(commentSchema)
+      .values({
+        content: input.content,
+        post: input.postId,
+        author: userId,
+        isAReply: input.isAReply,
+      })
+      .returning({ id: commentSchema.id });
+
+    await db
+      .update(commentSchema)
+      .set({
+        replies: sql`array_append(${commentSchema.replies}, ${userId})`,
+      })
+      .where(eq(commentSchema.id, input.replyTo));
+  } else {
+    comment = await db
+      .insert(commentSchema)
+      .values({
+        content: input.content,
+        post: input.postId,
+        author: userId,
+      })
+      .returning({ id: commentSchema.id });
+  }
+
+  await db
+    .update(postSchema)
+    .set({
+      commentsCount: sql`comments_count + 1`,
+    })
+    .where(eq(postSchema.id, input.postId));
+
+  return {
+    success: true,
+    message: "Comment added successfully",
+    commentId: comment[0]!.id,
+  };
+}
--- src/server/api/routers/postRouter.ts
+++ src/server/api/routers/postRouter.ts
@@ -1,36 +1,19 @@
-import { uploadPostSchema } from "@/lib/schemas";
+import * as postDal from "@/dal/post";
+import { commentAddSchema, uploadPostSchema } from "@/lib/schemas";
 import {
   createTRPCRouter,
   protectedProcedure,
   publicProcedure,
 } from "@/server/api/trpc";
-import { commentSchema, postSchema, userSchema } from "@/server/db/schema";
-import { desc, eq, sql } from "drizzle-orm";
 import { z } from "zod";
 
 export const postRouter = createTRPCRouter({
-  getFeed: publicProcedure.query(async ({ ctx }) => {
-    const results = await ctx.db
-      .select()
-      .from(postSchema)
-      .where(eq(postSchema.isPublic, true))
-      .leftJoin(userSchema, eq(userSchema.id, postSchema.createdBy))
-      .orderBy(desc(postSchema.createdAt));
-
-    return results.map((row) => ({
-      ...row.posts,
-      createdBy: row.users,
-    }));
+  getFeed: protectedProcedure.query(async () => {
+    return await postDal.getFeed();
   }),
 
   getUserPosts: protectedProcedure.query(async ({ ctx }) => {
-    const results = await ctx.db
-      .select()
-      .from(postSchema)
-      .where(eq(postSchema.createdBy, ctx.userId))
-      .orderBy(desc(postSchema.createdAt));
-
-    return results;
+    return await postDal.getLoggedInUserPost(ctx.userId);
   }),
 
   getPostById: publicProcedure
@@ -39,223 +22,56 @@ export const postRouter = createTRPCRouter({
         postId: z.string(),
       }),
     )
-    .query(async ({ input, ctx }) => {
-      const results = await ctx.db
-        .select()
-        .from(postSchema)
-        .where(eq(postSchema.id, input.postId));
-
-      const post = results[0];
-      if (!post) return null;
-
-      const userResults = await ctx.db
-        .select()
-        .from(userSchema)
-        .where(eq(userSchema.id, post.createdBy!));
-
-      const user = userResults[0] ?? null;
-
-      return {
-        ...post,
-        createdBy: user,
-      };
+    .query(async ({ input }) => {
+      return await postDal.getPostById(input.postId);
     }),
 
   createPost: protectedProcedure
     .input(uploadPostSchema)
-    .mutation(async ({ input, ctx }) => {
-      const post = await ctx.db
-        .insert(postSchema)
-        .values({
-          title: input.title,
-          content: input.content ?? null,
-          image: input.image ?? null,
-          isPublic: input.isPublic,
-          topic: input.topic ?? "",
-          createdBy: input.author,
-          likes: [],
-          disLikes: [],
-        })
-        .returning({ id: postSchema.id });
-
-      await ctx.db
-        .update(userSchema)
-        .set({
-          posts: sql`array_append(${userSchema.posts}, ${post[0]!.id})`,
-        })
-        .where(eq(userSchema.id, input.author));
-
-      if (Array.isArray(post) && post.length > 0 && post[0]?.id) {
-        return {
-          success: true,
-          message: "Post created successfully",
-          postId: post[0].id,
-        };
-      } else {
-        return {
-          success: false,
-          message: "Failed to create post",
-        };
-      }
+    .mutation(async ({ input }) => {
+      return await postDal.createPost(input);
     }),
 
   likePost: protectedProcedure
     .input(z.object({ post: z.string() }))
     .mutation(async ({ input, ctx }) => {
-      await ctx.db
-        .update(postSchema)
-        .set({
-          likes: sql`array_append(${postSchema.likes}, ${ctx.userId})`,
-        })
-        .where(eq(postSchema.id, input.post));
-
-      return {
-        success: true,
-        message: "Post Liked successfully",
-      };
+      return await postDal.likePost(ctx.userId, input.post);
     }),
   unlikePost: protectedProcedure
     .input(z.object({ post: z.string() }))
     .mutation(async ({ input, ctx }) => {
-      await ctx.db
-        .update(postSchema)
-        .set({
-          likes: sql`array_remove(${postSchema.likes}, ${ctx.userId})`,
-        })
-        .where(eq(postSchema.id, input.post));
-
-      return {
-        success: true,
-        message: "Post Unliked successfully",
-      };
+      return await postDal.unlikePost(ctx.userId, input.post);
     }),
   likeAndUndislikePost: protectedProcedure
     .input(z.object({ post: z.string() }))
     .mutation(async ({ input, ctx }) => {
-      await ctx.db
-        .update(postSchema)
-        .set({
-          likes: sql`array_append(${postSchema.likes}, ${ctx.userId})`,
-          disLikes: sql`array_remove(${postSchema.disLikes}, ${ctx.userId})`,
-        })
-        .where(eq(postSchema.id, input.post));
-
-      return {
-        success: true,
-        message: "Post liked and undisliked successfully",
-      };
+      return await postDal.likeAndUndislikePost(ctx.userId, input.post);
     }),
   dislikePost: protectedProcedure
     .input(z.object({ post: z.string() }))
     .mutation(async ({ input, ctx }) => {
-      await ctx.db
-        .update(postSchema)
-        .set({
-          disLikes: sql`array_append(${postSchema.disLikes}, ${ctx.userId})`,
-        })
-        .where(eq(postSchema.id, input.post));
-
-      return {
-        success: true,
-        message: "Post Disliked successfully",
-      };
+      return await postDal.dislikePost(ctx.userId, input.post);
     }),
   undislikePost: protectedProcedure
     .input(z.object({ post: z.string() }))
     .mutation(async ({ input, ctx }) => {
-      await ctx.db
-        .update(postSchema)
-        .set({
-          disLikes: sql`array_remove(${postSchema.disLikes}, ${ctx.userId})`,
-        })
-        .where(eq(postSchema.id, input.post));
-
-      return {
-        success: true,
-        message: "Post Undisliked successfully",
-      };
+      return await postDal.undislikePost(ctx.userId, input.post);
     }),
   dislikeAndUnlikePost: protectedProcedure
     .input(z.object({ post: z.string() }))
     .mutation(async ({ input, ctx }) => {
-      await ctx.db
-        .update(postSchema)
-        .set({
-          disLikes: sql`array_append(${postSchema.disLikes}, ${ctx.userId})`,
-          likes: sql`array_remove(${postSchema.likes}, ${ctx.userId})`,
-        })
-        .where(eq(postSchema.id, input.post));
-
-      return {
-        success: true,
-        message: "Post disliked and unliked successfully",
-      };
+      return await postDal.dislikeAndUnlikePost(ctx.userId, input.post);
     }),
 
   getCommentsByPostId: protectedProcedure
     .input(z.object({ postId: z.string() }))
-    .query(async ({ input, ctx }) => {
-      const results = await ctx.db
-        .select()
-        .from(commentSchema)
-        .where(eq(commentSchema.post, input.postId))
-        .orderBy(desc(commentSchema.createdAt))
-        .leftJoin(userSchema, eq(userSchema.id, commentSchema.author));
-
-      return results;
+    .query(async ({ input }) => {
+      return await postDal.getCommentsByPostId(input.postId);
     }),
 
   addComments: protectedProcedure
-    .input(
-      z.object({
-        content: z.string(),
-        postId: z.string(),
-        replyTo: z.string(),
-        isAReply: z.boolean().default(false),
-      }),
-    )
+    .input(commentAddSchema)
     .mutation(async ({ input, ctx }) => {
-      let comment;
-
-      if (input.isAReply) {
-        comment = await ctx.db
-          .insert(commentSchema)
-          .values({
-            content: input.content,
-            post: input.postId,
-            author: ctx.userId,
-            isAReply: input.isAReply,
-          })
-          .returning({ id: commentSchema.id });
-
-        await ctx.db
-          .update(commentSchema)
-          .set({
-            replies: sql`array_append(${commentSchema.replies}, ${ctx.userId})`,
-          })
-          .where(eq(commentSchema.id, input.replyTo));
-      } else {
-        comment = await ctx.db
-          .insert(commentSchema)
-          .values({
-            content: input.content,
-            post: input.postId,
-            author: ctx.userId,
-          })
-          .returning({ id: commentSchema.id });
-      }
-
-      await ctx.db
-        .update(postSchema)
-        .set({
-          commentsCount: sql`comments_count + 1`,
-        })
-        .where(eq(postSchema.id, input.postId));
-
-      return {
-        success: true,
-        message: "Comment added successfully",
-        commentId: comment[0]!.id,
-      };
+      return await postDal.addComment(input, ctx.userId);
     }),
 });
