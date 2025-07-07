3672ed634f6de841cbf5f3a0239a012f4bf79a87
Gurvinder Singh <avinashkaur842@gmail.com>
2025-07-07 19:41:29 +0530

    Enhance layout and styling for login and home pages
    - Updated the Home component to adjust the main container's styling for better alignment.
    - Introduced a new RootLayout component for the login page, including global styles and metadata setup.
    - Improved layout structure to center content effectively across different screen sizes.

---
+++ src/app/login/layout.tsx
@@ -0,0 +1,28 @@
+import "@/styles/globals.css";
+
+import { type Metadata } from "next";
+import { Geist } from "next/font/google";
+
+export const metadata: Metadata = {
+  title: "Social Media again",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className={`${geist.variable}`}>
+      <body>
+        <div className="flex justify-center lg:mr-64">
+          <div>{children}</div>
+        </div>
+      </body>
+    </html>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import SignUp from "../components/sign-up";
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen w-screen items-center justify-center">
+    <main className="flex min-h-screen w-screen items-center justify-center justify-self-end">
       <div className="flex min-w-[50vw] justify-center">
         <SignUp />
       </div>
