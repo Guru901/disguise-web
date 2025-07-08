9574c2978e2ef3e18f7d38a9562f8cef975c59d3
Gurvinder Singh <avinashkaur842@gmail.com>
2025-07-08 16:08:31 +0530

    Add user comments and disliked posts functionality to Me component
    - Integrated new API calls to fetch user comments and disliked posts.
    - Updated the Me component to display tabs for comments and disliked posts, enhancing user interaction.
    - Improved layout structure for better organization of user content.

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
 import { Loader2 } from "lucide-react";
@@ -27,6 +27,12 @@ export default function Me() {
 
   const { data: userPrivatePosts, isLoading: isPrivatePostsLoading } =
     api.postRouter.getLoggedInUserPrivatePosts.useQuery();
+
+  const { data: userComments, isLoading: isCommentsLoading } =
+    api.postRouter.getLoggedInUserComments.useQuery();
+
+  const { data: userDisLikedPosts, isLoading: isDisLikedPostsLoading } =
+    api.postRouter.getLoggedInUserDisLikedPosts.useQuery();
 
   const user = data?.user;
   const username = user?.username ?? "User";
@@ -110,34 +116,59 @@ export default function Me() {
                 </div>
               )}
             </div>
-            <div className="mt-2 flex w-full flex-wrap gap-2 lg:mt-0">
-              <Tabs
-                defaultValue="public"
-                value={selectedOption}
-                className="w-full"
-                onValueChange={(e) => setSelectedOption(e)}
-              >
-                <TabsList className="w-full">
-                  <TabsTrigger
-                    value={"public"}
-                    className={`${!isFriend ? "w-1/2" : "w-1/3"}`}
-                  >
-                    Public Posts ({userPosts?.length ?? 0})
-                  </TabsTrigger>
-                  <TabsTrigger
-                    value={"liked"}
-                    className={`${!isFriend ? "w-1/2" : "w-1/3"}`}
-                  >
-                    Liked Posts ({userLikedPosts?.length ?? 0})
-                  </TabsTrigger>
-                  {isFriend ||
-                    (isProfile && (
-                      <TabsTrigger value={"private"} className="w-1/3">
-                        Private Posts ({userPrivatePosts?.length ?? 0})
-                      </TabsTrigger>
-                    ))}
-                </TabsList>
-              </Tabs>
+            <div className="flex w-full flex-col gap-2">
+              <div className="mt-2 flex w-full flex-wrap gap-2 lg:mt-0">
+                <Tabs
+                  defaultValue="public"
+                  value={selectedOption}
+                  className="w-full"
+                  onValueChange={(e) => setSelectedOption(e)}
+                >
+                  <TabsList className="w-full">
+                    <TabsTrigger
+                      value={"public"}
+                      className={`${!isFriend ? "w-1/2" : "w-1/3"}`}
+                    >
+                      Public Posts ({userPosts?.length ?? 0})
+                    </TabsTrigger>
+                    <TabsTrigger
+                      value={"liked"}
+                      className={`${!isFriend ? "w-1/2" : "w-1/3"}`}
+                    >
+                      Liked Posts ({userLikedPosts?.length ?? 0})
+                    </TabsTrigger>
+                    {isFriend ||
+                      (isProfile && (
+                        <TabsTrigger value={"private"} className="w-1/3">
+                          Private Posts ({userPrivatePosts?.length ?? 0})
+                        </TabsTrigger>
+                      ))}
+                  </TabsList>
+                </Tabs>
+              </div>
+              <div className="flex w-full flex-wrap gap-2 lg:mt-0">
+                <Tabs
+                  defaultValue="public"
+                  value={selectedOption}
+                  className="w-full"
+                  onValueChange={(e) => setSelectedOption(e)}
+                >
+                  <TabsList className="w-full">
+                    <TabsTrigger
+                      value={"disLiked"}
+                      className={`${!isFriend ? "w-1/2" : "w-1/3"}`}
+                    >
+                      Disliked Posts ({userDisLikedPosts?.length ?? 0})
+                    </TabsTrigger>
+                    <TabsTrigger
+                      value={"comments"}
+                      className={`${!isFriend ? "w-1/2" : "w-1/3"}`}
+                    >
+                      Comments ({userComments?.length ?? 0})
+                    </TabsTrigger>
+                  </TabsList>
+                </Tabs>
+              </div>
             </div>
           </div>
         </div>
@@ -227,6 +258,60 @@ export default function Me() {
                 </div>
               ) : (
                 userPrivatePosts?.slice().map((post) => (
+                  <Card key={post.id} className="overflow-hidden">
+                    <Link href={`/p/${post.id}`} className="h-full">
+                      <CardContent className="h-full p-0">
+                        {post.image ? (
+                          <div className="relative h-full">
+                            <div className="absolute top-0 right-0 bottom-0 left-0 rounded-xl bg-black/40">
+                              <div className="flex h-full w-full items-center justify-center rounded-xl text-xl text-white opacity-100">
+                                <h1>{post.title}</h1>
+                              </div>
+                            </div>
+                            <Image
+                              src={post.image}
+                              alt="Post"
+                              width={500}
+                              height={300}
+                              className="h-full w-full rounded-xl object-cover"
+                            />
+                          </div>
+                        ) : (
+                          <div className="bg-secondary flex h-[500px] w-full items-center justify-center rounded-lg text-xl text-white">
+                            <h1>{post.title}</h1>
+                          </div>
+                        )}
+                      </CardContent>
+                    </Link>
+                  </Card>
+                ))
+              )
+            ) : selectedOption === "comments" ? (
+              isCommentsLoading ? (
+                <div className="flex h-full w-full items-center justify-center">
+                  <Loader2 className="animate-spin" size={20} />
+                </div>
+              ) : (
+                userComments?.map((comment) => (
+                  <Card key={comment.id} className="overflow-hidden">
+                    <Link
+                      href={`/p/${comment.post}?comment=${comment.id}`}
+                      className="h-full"
+                    >
+                      <CardContent className="h-full p-2">
+                        <h1>{comment.content}</h1>
+                      </CardContent>
+                    </Link>
+                  </Card>
+                ))
+              )
+            ) : selectedOption === "disLiked" ? (
+              isDisLikedPostsLoading ? (
+                <div className="flex h-full w-full items-center justify-center">
+                  <Loader2 className="animate-spin" size={20} />
+                </div>
+              ) : (
+                userDisLikedPosts?.map((post) => (
                   <Card key={post.id} className="overflow-hidden">
                     <Link href={`/p/${post.id}`} className="h-full">
                       <CardContent className="h-full p-0">
