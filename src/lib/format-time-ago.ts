374ef8e61f2299018e7d836af723497ff0c10f79
Gurvinder Singh <avinashkaur842@gmail.com>
2025-07-18 19:12:58 +0530

    Improve time ago formatting for singular and plural cases in formatTimeAgo function

--- src/lib/format-time-ago.ts
+++ src/lib/format-time-ago.ts
@@ -4,16 +4,21 @@ export function formatTimeAgo(date: Date) {
     (new Date().getTime() - parsedDate.getTime()) / 1000,
   );
   let interval = seconds / 31536000;
-  if (interval > 1) return `${Math.floor(interval)} yrs ago`;
+  if (interval > 1)
+    return `${Math.floor(interval)} yr${interval === 1 ? "" : "s"} ago`;
   interval = seconds / 2592000;
   if (interval > 1) return `${Math.floor(interval)} months ago`;
   interval = seconds / 604800;
-  if (interval > 1) return `${Math.floor(interval)} weeks ago`;
+  if (interval > 1)
+    return `${Math.floor(interval)} week${Math.floor(interval) === 1 ? "" : "s"} ago`;
   interval = seconds / 86400;
-  if (interval > 1) return `${Math.floor(interval)} days ago`;
+  if (interval > 1)
+    return `${Math.floor(interval)} day${Math.floor(interval) === 1 ? "" : "s"} ago`;
   interval = seconds / 3600;
-  if (interval > 1) return `${Math.floor(interval)} hrs ago`;
+  if (interval > 1)
+    return `${Math.floor(interval)} hr${Math.floor(interval) === 1 ? "" : "s"} ago`;
   interval = seconds / 60;
-  if (interval > 1) return `${Math.floor(interval)} mins ago`;
+  if (interval > 1)
+    return `${Math.floor(interval)} min${Math.floor(interval) === 1 ? "" : "s"} ago`;
   return "Just now";
 }
